005b31520c0103ee145431655bddc43cc7ee4dc7
tasyashalfirataufiq <tasyashalfirataufiq@gmail.com>
2023-12-12 13:23:07 +0700

    revisi segitiga

--- src/Kelas.java
+++ src/Kelas.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
 
-class Segitiga {
+class Triangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input tinggi segitiga: ");
+        System.out.print("Enter the height of the triangle: ");
 
-        int tinggi = scanner.nextInt();
-        int tinggi_2 = tinggi;
+        int height = scanner.nextInt();
+        int currentHeight = height;
 
-        for (int i = 1; i <= tinggi * 2; i++) {
-            if (i <= tinggi){
-                for (int j = 1; j <= i; j++) {
+        for (int row = 1; row <= height * 2; row++) {
+            if (row <= height) {
+                for (int stars = 1; stars <= row; stars++) {
                     System.out.print("* ");
                 }
             } else {
-                for (int j = tinggi_2; j >= 1; j--) {
+                for (int stars = currentHeight; stars >= 1; stars--) {
                     System.out.print("* ");
                 }
-                tinggi_2 -= 1;
+                currentHeight -= 1;
             }
             System.out.println();
         }
         scanner.close();
     }
 }
+
